a668972d1080eafd8d2eaa835bee26db3dab4bd2
배정민 <baejm@fasol.co.kr>
2025-02-18 16:49:00 +0900

    회원가입 기능 작성

---
+++ src/main/kotlin/com/example/demo/member/repository/MemberRepository.kt
@@ -0,0 +1,10 @@
+package com.example.demo.member.repository
+
+import com.example.demo.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByLoginId(loginId: String): Member?
+}
---
+++ src/test/kotlin/com/example/demo/member/service/MemberServiceTest.kt
@@ -0,0 +1,37 @@
+package com.example.demo.member.service
+
+import com.example.demo.common.status.Gender
+import com.example.demo.member.dto.MemberDtoRequest
+import com.example.demo.member.entity.Member
+import com.example.demo.member.repository.MemberRepository
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.json.JsonWriter
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+import java.util.*
+
+@SpringBootTest
+class MemberServiceTest @Autowired constructor(
+    private val memberRepository: MemberRepository,
+    private val memberService: MemberService
+) {
+    @Test
+    fun signUp() {
+        memberService.signUp(
+            MemberDtoRequest(
+                null,
+                "testId",
+                "testPassword",
+                "홍길동",
+                LocalDate.now(),
+                Gender.MAN,
+                "hello@world.com"
+            )
+        )
+
+     assertTrue(Optional.ofNullable(memberRepository.findByLoginId("testId")).isPresent)
+    }
+}
