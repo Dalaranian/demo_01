f50bd2b53041e07c89bd2986f9d6ddba054c0bd5
배정민 <baejm@fasol.co.kr>
2025-02-18 15:33:55 +0900

    modify applcation.yml / build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	val kotlinVersion = "1.9.25"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("plugin.jpa") version kotlinVersion
 }
 
 group = "com.example"
