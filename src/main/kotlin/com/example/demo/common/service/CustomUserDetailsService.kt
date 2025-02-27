1f98cdad804c3c99c38eae8fa60122db0913fb49
배정민 <baejm@fasol.co.kr>
2025-02-27 13:19:10 +0900

    feat : create CustomUser

--- src/main/kotlin/com/example/demo/common/service/CustomUserDetailsService.kt
+++ src/main/kotlin/com/example/demo/common/service/CustomUserDetailsService.kt
@@ -1,5 +1,6 @@
 package com.example.demo.common.service
 
+import com.example.demo.common.dto.CustomUser
 import com.example.demo.member.entity.Member
 import com.example.demo.member.repository.MemberRepository
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -22,10 +23,11 @@ class CustomUserDetailsService(
     }
 
     private fun createUserDetails(member: Member): UserDetails {
-        return User(
-                    member.loginId,
-                    passwordEncoder.encode(member.password),
-                    member.memberRole!!.map { SimpleGrantedAuthority("ROLE_${it.role}") }
-                )
+        return CustomUser(
+            member.id!!,
+            member.loginId,
+            passwordEncoder.encode(member.password),
+            member.memberRole!!.map { SimpleGrantedAuthority("ROLE_${it.role}") }
+        )
     }
 }
