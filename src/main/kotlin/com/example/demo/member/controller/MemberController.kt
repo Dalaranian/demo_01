5dc3f43da2f73725a46d35bc0221a7e0e5eb3d44
배정민 <baejm@fasol.co.kr>
2025-02-28 10:53:03 +0900

    Feat : create SaveMyInfo

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -9,12 +9,7 @@ import com.example.demo.member.dto.MemberDtoResponse
 import com.example.demo.member.service.MemberService
 import jakarta.validation.Valid
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/api/member")
 @RestController
@@ -48,5 +43,16 @@ class MemberController(
         val userId = (SecurityContextHolder.getContext().authentication.principal as CustomUser).userId
         val response = memberService.searchMyInfo(userId)
         return BaseResponse(data = response)
+    }
+
+    /**
+     * 내 정보 수정
+     */
+    @PutMapping("/info")
+    fun saveMyInfo(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<Unit> {
+        val userId = (SecurityContextHolder.getContext().authentication.principal as CustomUser).userId
+        memberDtoRequest.id = userId
+        val resultMessage = memberService.saveMyInfo(memberDtoRequest)
+        return BaseResponse(message = resultMessage)
     }
 }
